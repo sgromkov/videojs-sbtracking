0ad89071f586459bd799640355e6841c2d04a284
Sergey Gromkov <sgromkov@gmail.com>
2017-11-24 18:51:29 +0300

    Create plugin

---
+++ README.md
@@ -0,0 +1,104 @@
+# videojs-sbtracking
+
+
+
+## Table of Contents
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+## Installation
+
+- [Installation](#installation)
+- [Usage](#usage)
+  - [`<script>` Tag](#script-tag)
+  - [Browserify/CommonJS](#browserifycommonjs)
+  - [RequireJS/AMD](#requirejsamd)
+- [License](#license)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+## Installation
+
+```sh
+npm install --save videojs-sbtracking
+```
+
+## Usage
+
+To include videojs-sbtracking on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="//path/to/video.min.js"></script>
+<script src="//path/to/videojs-sbtracking.min.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.sbtracking({
+    mediaStateCode: "",
+    mediaId: "",
+    playeri: "",
+    siteOwnerId: "",
+    mainRubric: "",
+    hasAds: false,
+    videoUrl: "",
+    url: "/sbtracking/vplayer"
+  });
+</script>
+```
+
+### Browserify/CommonJS
+
+When using with Browserify, install videojs-sbtracking via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-sbtracking');
+
+var player = videojs('my-video');
+
+player.sbtracking({
+  mediaStateCode: "",
+  mediaId: "",
+  playeri: "",
+  siteOwnerId: "",
+  mainRubric: "",
+  hasAds: false,
+  videoUrl: "",
+  url: "/sbtracking/vplayer"
+});
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-sbtracking'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.sbtracking({
+    mediaStateCode: "",
+    mediaId: "",
+    playeri: "",
+    siteOwnerId: "",
+    mainRubric: "",
+    hasAds: false,
+    videoUrl: "",
+    url: "/sbtracking/vplayer"
+  });
+});
+```
+
+## License
+
+MIT. Copyright (c) Sergey Gromkov &lt;sgromkov@gmail.com&gt;
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,148 @@
+import videojs from 'video.js';
+import {version as VERSION} from '../package.json';
+
+// Default options for the plugin.
+const defaults = {};
+
+// Cross-compatibility for Video.js 5 and 6.
+const registerPlugin = videojs.registerPlugin || videojs.plugin;
+// const dom = videojs.dom || videojs;
+
+/**
+ * Class representing an sbtracking plugin
+ */
+class Sbtracking {
+  /**
+   * Plugin initialization
+   *
+   * @param    {Player} player
+   *           A Video.js player object.
+   *
+   * @param    {Object} [options={}]
+   *           A plain object containing options for the plugin.
+   */
+  constructor(player, options) {
+    this.player = player;
+    this.options = options;
+    this.events = [];
+  }
+
+  /**
+   * Will send tracking event data to server
+   *
+   * @function postTrackingEvent
+   * @param {string} name
+   *        Event name
+   * @param {Object} params
+   *        Event params
+   */
+  postTrackingEvent(name, params) {
+    if (!name.match(/buffer/i) && (typeof this.events[name] !== 'undefined')) {
+      return;
+    }
+
+    if (typeof params !== 'object') {
+      videojs.log('action_params must be an object');
+      return;
+    }
+
+    const urlParams = [];
+
+    urlParams.push('action_name=' + encodeURIComponent(name));
+    urlParams.push('from=' + encodeURIComponent(document.referrer));
+    urlParams.push('media_state_code=' + this.options.mediaStateCode);
+    urlParams.push('media_id=' + this.options.mediaId);
+    urlParams.push('player_version=7');
+    if (this.options.playeri !== null) {
+      urlParams.push('playeri=' + this.options.playeri);
+    }
+    urlParams.push('has_adblock=' + 0);
+    urlParams.push('site_owner_id=' + this.options.siteOwnerId);
+    urlParams.push('main_rubric=' + this.options.mainRubric);
+
+    let flashVersion = -1;
+
+    if (typeof swfobject !== 'undefined') {
+      flashVersion = swfobject.getFlashPlayerVersion().major;
+    }
+
+    urlParams.push('flash_version=' + flashVersion);
+
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        const param = key + '=' + encodeURIComponent(params[key]);
+
+        urlParams.push(param);
+      }
+    }
+
+    const url = this.options.url + '?' + urlParams.join('&');
+
+    if (window.XDomainRequest) {
+      const xdr = new XDomainRequest();
+
+      xdr.open('POST', url);
+      xdr.send(null);
+    } else {
+      const xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.open('POST', url, true);
+      xmlhttp.send(null);
+    }
+
+    this.events[name] = 1;
+  }
+
+  /**
+   * Function to invoke when the player is ready.
+   *
+   * When this function is called, the player
+   * will have its DOM and child components in place.
+   *
+   * @function ready
+   */
+  ready() {
+    videojs.log('sbtracking Plugin ENABLED!', this.options);
+
+    this.player.addClass('vjs-videojs-sbtracking');
+
+    this.player.on('loadstart', () => {
+      this.postTrackingEvent('player_show', {});
+      if (this.options.hasAds) {
+        document.dispatchEvent(new Event('wjplayerWithAdsLoadstart'));
+      }
+    });
+
+    this.player.one('play', () => {
+      this.postTrackingEvent('content_play', {file: this.options.videoUrl});
+    });
+  }
+}
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function sbtracking
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const sbtracking = function(options) {
+  const tracker = new Sbtracking(this, videojs.mergeOptions(defaults, options));
+
+  this.ready(() => {
+    tracker.ready();
+  });
+};
+
+// Register the plugin with video.js.
+registerPlugin('sbtracking', sbtracking);
+
+// Include the version number.
+sbtracking.VERSION = VERSION;
+
+export default sbtracking;
